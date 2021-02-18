787075064ab63018a09817ee31afc23f2b5f2d85
Gustavo Segovia <gustavo.segovia03gmail.com>
2021-02-18 10:59:54 -0300

    context api clase 05 reducer,state,actions

--- src/components/Context/Cart/CartProvider.jsx
+++ src/components/Context/Cart/CartProvider.jsx
@@ -1,7 +1,33 @@
-import React, { createContext } from "react";
+import { useReducer } from "react";
+import { ADD_TO_CART, REMOVE_FROM_CART } from "./actions";
+import CartContext from "./CartContext";
 
-const CartProvider = () => (
- <CartContext.Provider value={}></CartContext.Provider>
-);
+const initialState = {
+ cart: [],
+};
+
+const cartreducer = (state, { type, course }) => {
+ if (type === ADD_TO_CART) {
+  return {
+   cart: state.cart.concat(course),
+  };
+ }
+
+ if (type === REMOVE_FROM_CART) {
+  return {
+   cart: state.cart.filter(c => c !== course),
+  };
+ }
+ return state;
+};
+
+const CartProvider = ({ children }) => {
+ const [state, dispatch] = useReducer(cartreducer, initialState);
+ return (
+  <CartContext.Provider value={[state, dispatch]}>
+   {children}
+  </CartContext.Provider>
+ );
+};
 
 export default CartProvider;
